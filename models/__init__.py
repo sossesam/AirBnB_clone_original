1a83207a4ad3dec81a18acd85619b149a738f12d
Seunelvis1 <adeniranoluwaseunelvis@gmail.com>
2023-10-15 06:54:04 -0700

    console et al

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,8 @@ Instantiation file that starts the file storage system
 """
 
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+sotrage.reload()
