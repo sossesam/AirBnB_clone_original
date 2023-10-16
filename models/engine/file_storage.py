c935a4b017aa5f72e64018f3521e456e56125c93
oyeniran samuel sanmi <sossesam@gmail.com>
2023-10-16 07:56:31 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,46 +11,36 @@ from models.state import State
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
 
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns a dictionary of objects"""
-
+       
         return type(self).__objects
 
     def new(self, obj):
-        """ Sets in objects with key to self.__objects
-        Args:
-            obj (dict): objects to set self.__objects with.
-        """
-        objNameId = obj.__class__.__name__ + "." + obj.id
-        type(self).__objects[objNameId] = obj
+       
+        id = obj.__class__.__name__ + "." + obj.id
+        type(self).__objects[id] = obj
 
     def save(self):
-        """ Serializes objects to Json file specified by file path"""
-
+        
         with open(type(self).__file_path, "w", encoding='utf-8') as file:
-            dict_storage = {}
+            new_dict = {}
             for key, val in type(self).__objects.items():
-                dict_storage[key] = val.to_dict()
-            json.dump(dict_storage, file)
+                new_dict[key] = val.to_dict()
+            json.dump(new_dict, file)
 
     def reload(self):
-        """Deserializes the Json file to objects if it exists"""
+      
         try:
             with open(type(self).__file_path, encoding='utf-8') as file:
-                objdict = json.load(file)
-                for obj in objdict.values():
-                    cls_name = obj["__class__"]
-                    del obj["__class__"]
-                    self.new(eval(cls_name)(**obj))
+                obj_dict = json.load(file)
+                for obj in obj_dict.values():
+                    class_name = obj_dict["__class__"]
+                    del obj_dict["__class__"]
+                    self.new(eval(class_name)(**obj))
         except FileNotFoundError:
             return
